37799a5d1dfa793a484a7d7d6edc7d786670a1a1
Yash Saravgi <f20191530@hyderabad.bits-pilani.ac.in>
2021-01-21 18:32:53 +0530

    last

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const url = 'https://bitsagram.herokuapp.com';
+const API = axios.create({ baseURL: 'https://bitsagram.herokuapp.com' });
 
-url.interceptors.request.use((req) => {
+API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
     req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
   }
@@ -10,10 +10,11 @@ url.interceptors.request.use((req) => {
   return req;
 });
 
-export const fetchPosts = () => axios.get(`${url}/posts`);
-export const createPost = (newPost) => axios.post(url, newPost);
-export const likePost = (id) => axios.patch(`${url}/posts/${id}/likePost`);
-export const updatePost = (id, updatedPost) => axios.patch(`${url}/posts/${id}`, updatedPost);
-export const deletePost = (id) => axios.delete(`${url}/posts/${id}`);
-export const signIn = (formData) => axios.post(`${url}/user/signin`, formData);
-export const signUp = (formData) => axios.post(`${url}/user/signup`, formData);
+export const fetchPosts = () => API.get('/posts');
+export const createPost = (newPost) => API.post('/posts', newPost);
+export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
+export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
+export const deletePost = (id) => API.delete(`/posts/${id}`);
+
+export const signIn = (formData) => API.post('/user/signin', formData);
+export const signUp = (formData) => API.post('/user/signup', formData);
